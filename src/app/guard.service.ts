01c3534be2ae10298749138c62eaa62503011c69
ArthurMauricioDelgadillo <amd11dot4@gmail.com>
2016-11-02 03:52:03 +0000

    feat(auth): guard redirectURL

--- src/app/guard.service.ts
+++ src/app/guard.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, } from '@angular/router';
+import { AuthService } from './auth.service';
+import { CanActivate, Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 
 @Injectable()
 export class GuardService implements CanActivate {
-  constructor(private router: Router) { }
-  canActivate() {
-    if (!!window.localStorage.getItem('user') && !!window.localStorage.getItem('feathers-jwt')) {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    let url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  private checkLogin(url: string): boolean {
+    if (this.auth.isLoggedIn) {
       return true;
     } else {
+      // Store the attempted URL for redirecting
+      this.auth.redirectURL = url;
+      console.log(url);
+      // Navigate to the login page with extras
       this.router.navigate(['/login']);
       return false;
     }
