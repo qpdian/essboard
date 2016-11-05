5d35ee494b8368e3bafa5f2162ce818e948d7f1a
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-11-05 10:13:05 -0500

    refactor(app): layout move to app

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,38 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
+import { ValidationMessagesModule } from 'ng2-custom-validation';
 
 // TODO: every module has to import it's own form or reactive module
 import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
-import { ValidationMessagesModule, ValidationMessagesLoader } from 'ng2-custom-validation';
-import { messageLoaderFactory } from './messages/loader';
-// TODO: refactor this, just header component use this, move to layouts
-import { RouterModule } from '@angular/router'; 
-
-import { HeaderComponent } from './components/header.component';
 
 @NgModule({
   imports: [
       CommonModule,
+
       FormsModule,
-      ReactiveFormsModule,
-      MaterialModule.forRoot(),
-      RouterModule,
-      ValidationMessagesModule.forRoot({
-            provide: ValidationMessagesLoader,
-            useFactory: messageLoaderFactory
-        }),
+      ReactiveFormsModule
   ],
   exports: [
       CommonModule,
-      FormsModule,
-      ReactiveFormsModule,
       MaterialModule,
-      HeaderComponent,
-      ValidationMessagesModule
-  ],
-  declarations: [
-      HeaderComponent
+      ValidationMessagesModule,
+
+
+      FormsModule,
+      ReactiveFormsModule
   ]
 })
 export class SharedModule { }
