3f598a0fd8c566e6d43aa0f80cc055db8f8c6e2a
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-11-09 00:54:29 -0500

    chore(env): to prod env

--- src/app/env.ts
+++ src/app/env.ts
@@ -2,6 +2,6 @@ export const env = {
     api: {
         second : 'https://essboard-backend-marthur.c9users.io/',
         local : 'http://localhost:8080',
-        main : 'https://essboard-backend-marthur.c9users.io/',//'https://essboard-backend.herokuapp.com/',
+        main : 'https://essboard-backend.herokuapp.com/',
     }
 };
